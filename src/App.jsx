c357657a776ae550b0abe10ac9785ccc37f1ef59
iosebkhe <ioseb.khetsuriani@yahoo.com>
2023-10-05 23:04:17 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import Stats from "./components/Stats";
+
+const App = function () {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <TodoList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+};
+
+export default App;
